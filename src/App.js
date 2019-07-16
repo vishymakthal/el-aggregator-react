63ae1cdd54091e4ae2b023bfc8b287ec56cee59f
vishymakthal <vmaktha@ncsu.edu>
2019-07-15 23:32:11 -0400

    Initial commit to Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import algoliasearch from 'algoliasearch';
+import {InstantSearch, SearchBox, Hits, Highlight} from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch(
+  process.env.APP_ID,
+  process.env.APP_KEY
+);
+
+const Hit = ({ hit }) => (
+      <div key={hit.objectID}><h3>{hit.name}</h3><br/><img src={hit.img}/><p>{hit.club}</p></div>
+    );
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="ais-InstantSearch">
+        <h1>Player Search Test</h1>
+        <InstantSearch indexName="players" searchClient={searchClient}>
+          <div className="right-panel">
+            <SearchBox />
+            <Hits hitComponent={Hit} />
+          </div>
+        </InstantSearch>
       </div>
     );
   }
